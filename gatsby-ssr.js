80486f0ff4a8a7a43e7f9ad85e8e4f6e229bbf41
Pedram Navid <pedram.navid@gmail.com>
2022-08-06 18:25:15 -0700

    Remove gatsby browser

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,7 +13,7 @@ export function onRenderBody({ setHeadComponents }, pluginOptions) {
   const snippet = `window.jitsu = window.jitsu || (function(){(window.jitsuQ = window.jitsuQ || []).push(arguments);})`
 
   // only render snippet if write key exists
-  if (writeKey) {
+  if (key) {
     setHeadComponents([
       <script key="plugin-jitsu" defer src={host} data-key={key}></script>,
       <script dangerouslySetInnerHTML={{ __html: snippet }}></script>
